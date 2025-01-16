d2803636c8a9174f76c888e6101a1d8f816e23af
nikcani <niklascanisius@me.com>
2025-01-16 16:57:11 +0100

    processCssUrls remove the font files > add back again & include scss since paths work now too

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require("laravel-mix");
 
 mix.setPublicPath("dist");
 
-mix.copy("src/img", "dist/img");
+mix.copy("src", "dist");
 
 mix.sass("src/colors.scss", "/");
 mix.sass("src/fonts.scss", "/").options({ processCssUrls: false });
